0337c77edd75266297b0e19c596b219491dee9d1
Samaun <91575341+samaun25@users.noreply.github.com>
2023-09-16 17:17:37 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/a2156e8252.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style1.css">
+    <title>Project 1</title>
+</head>
+<body>
+    <div class="header">
+        <div class="side-nav">
+            <a href=""class="logo">
+                <img src="">
+            </a>
+            <ul class="nav-list">
+                <li><a href="http://localhost:8000/Task%202.php"><i class="fa-solid fa-house"></i>  <p> Home</p></a></li>
+                <li><a href="http://localhost:8000/Task%201.php"><i class="fa-solid fa-person-through-window"></i><p>Personal</p></a></li>
+                <li><a href="http://localhost:8000/Task%202.php"><i class="fa-solid fa-temperature-three-quarters"></i><p>Temperature</p></a></li>
+                <li><a href="http://localhost:8000/Task%203.php"><i class="fa-solid fa-user-graduate"></i><p>Grade</p></a></li>
+                <li><a href="http://localhost:8000/Task%204.php"><i class="fa-solid fa-equals"></i><p>Even or Odd</p></a></li>
+                <li><a href="http://localhost:8000/Task%205.php"><i class="fa-solid fa-cloud"></i><p>Weather</p></a></li>
+                <li><a href="http://localhost:8000/Task%206.php"><i class="fa-solid fa-comment"></i><p>Comparison</p></a></li>
+                <li><a href="http://localhost:8000/Task%207.php"><i class="fa-solid fa-calculator"></i><p>Calculator</p></a></li>
+                 <div class="active"></div>
+            </ul>
+        </div>
+
+    </div>
+</body>
+</html>
